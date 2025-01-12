ddd1dc94fbfa16522a3d90740524576f1eb77cbc
JohnGillien <145457180+JohnGillien@users.noreply.github.com>
2025-01-12 20:30:38 +0100

    Update Dia10.cpp

--- Dia10.cpp
+++ Dia10.cpp
@@ -6,31 +6,82 @@
 #include <cstring>
 
 using namespace std;
-
+int id = 1000;
 vector<vector<int>> a;
 vector<pair<int, int>> dirs{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+vector<vector<bool>> vis;
 
-int buscacamino(int x, int y, vector<vector<int>>& dp) {
+int W, H;
+class pu{
+    public:
+    int pt;
+    int id;
+};
+int buscacamino(int x, int y, vector<vector<pu>>& dp) {
+
     if (x < 0 || y < 0 || x >= a.size() || y >= a[0].size()) {
         return 0;
     }
-    if (dp[x][y] > 0) {
-        return dp[x][y];
+    if (dp[x][y].pt > 0) {
+        return dp[x][y].pt;
     }
     if (a[x][y] == 9) {
-        return dp[x][y] = 1;
+        dp[x][y].pt=1;
+        dp[x][y].id= id;
+        id--;
+        cout << id << endl;
+        return dp[x][y].pt;
     }
     
     int total = 0;
+    int id1, id2, id3, id4;
+    int v1=0, v2=0, v3=0, v4=0;
+    int i=0;
     for (pair<int, int> dir : dirs) {
         int r2 = x + dir.first;
         int c2 = y + dir.second;
         // Verificar que r2 y c2 están dentro de los límites antes de acceder a a[r2][c2]
         if (r2 >= 0 && r2 < a.size() && c2 >= 0 && c2 < a[0].size() && a[r2][c2] == a[x][y] + 1) {
-            total += buscacamino(r2, c2, dp);
+            switch(i){
+                case 0:
+                    v1=buscacamino(r2, c2, dp);
+                    id1=dp[r2][c2].id;
+                    dp[x][y].id=id1;
+                    total+=v1;
+                    break;
+                case 1:
+                    v2=buscacamino(r2, c2, dp);
+                    id2=dp[r2][c2].id;
+                    dp[x][y].id=id2;
+                    total+=v2;
+                    break;
+                case 2:
+                    v3=buscacamino(r2, c2, dp);
+                    id3=dp[r2][c2].id;
+                    dp[x][y].id=id3;
+                    total+=v3;
+                    break;
+                case 3:
+                    v4=buscacamino(r2, c2, dp);
+                    id4=dp[r2][c2].id;
+                    dp[x][y].id=id4;
+                    total+=v4;
+                    break;
+            }
         }
+        i++;
     }
-    return dp[x][y] = total;
+    if( id1==id2 || id1==id3 || id1==id4){
+            total-=v1;
+        }
+        else if( id2==id3 || id2==id4){
+            total-=v2;
+        }
+        else if( id3==id4){
+            total-=v3;
+        }
+    dp[x][y].pt = total;
+    return dp[x][y].pt;
 }
 
 int main() {
@@ -69,7 +120,7 @@ int main() {
     }
 
     // Inicializar la matriz dp a ceros
-    vector<vector<int>> dp(a.size(), vector<int>(a[0].size(), 0));
+    vector<vector<pu>> dp(a.size(), vector<pu>(a[0].size(), {0, 0}));
 
     int total_score = 0;
     for (int row = 0; row < a.size(); row++) {
